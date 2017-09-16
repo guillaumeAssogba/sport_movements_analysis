80f0aa02f30e66a23a4561a016c3273ae971f55a
gassogba <guillaume.Assogba@eleves.ec-nantes.fr>
2017-09-16 16:39:22 +0100

    ﻿Beginning the creation of a report output with latex

--- latexRendering.py
+++ latexRendering.py
@@ -1,4 +1,4 @@
-from pylatex import Document, Section, Subsection, Command, Figure
+from pylatex import Document, Section, Subsection, Command, Figure, Table, Tabular
 from pylatex.utils import italic, NoEscape
 import os
 
@@ -19,20 +19,53 @@ def fillLoadingsregression(doc, loadings):
     with doc.create(Subsection('Regressions using PCA\'s loadings and performance ')):
         with doc.create(Figure(position='h!')) as loadings_pic:
             for i in range(3):
-                loadings_pic.add_image(loadings[i], width='200px')
-            loadings_pic.add_caption('Title 1')  
+                loadings_pic.add_image(loadings[i], width='150px')
+            loadings_pic.add_caption('Title 1')
+            
+def varianceExplainedTable(doc, pcs, name ):
+    doc.append('By applying the PCA to the sportsmen\'s dataset, the first three PCs were computed and explain more than 95\% of the variance for each body part.')
+    doc.append('The percentage of variance explained by the PCs are shown in Table 1.')
+    with doc.create(Table(position='h!')) as table:
+        with doc.create(Tabular('ccc')) as tabular:
+            tabular.add_hline()
+            tabular.add_row(('Variable', 'PC number', 'Variance explained'))
+            
+            for i in range(len(pcs)):
+                tabular.add_hline()
+                tabular.add_row((name[i],'',''))
+                tabular.add_hline()
+                for j in range(len(pcs[0])):
+                    tabular.add_row(('','PC'+str(j+1),pcs[i][j]))
+        table.add_caption("Percent variation explained for the first "+ str(len(pcs[0])) +" Principle Components of the rugby players’ body variables")
 
-def fillPca(doc, loadings):
+def PCAfigures(doc, pca):
+    doc.append('The representation of the projections of the data along the three axes is then performed as represented in the Figure 1.')
+    with doc.create(Figure(position='h!')) as pca_pic:
+        for i in range(3):
+            pca_pic.add_image(pca[i], width='150px')
+        pca_pic.add_caption('Projection of the PC scores of respectively the variable A, variable B and variable C')
+    
+def fillPCArepresentation(doc, pca, pcs, name):
+    with doc.create(Subsection('PCA application ')):
+        varianceExplainedTable(doc, pcs, name)
+        PCAfigures(doc, pca)
+        
+def fillPca(doc, loadings, pcs, name):
     with doc.create(Section('Statistical Analysis')):
-        doc.append('This part describe the different results obtaining by\
-                  applying the PrinciPal Components analysis to the dataset')
-    fillLoadingsregression(doc, loadings)
+        doc.append('This part describe the different results obtaining by applying the PrinciPal Components analysis to the body movement dataset. ')
+    fillPCArepresentation(doc, pca, pcs, name)
+    #fillLoadingsregression(doc, loadings)
 
 if __name__ == '__main__':
+    nb = 3
     loadings = []
-    loadings.append(os.path.join(os.path.dirname(__file__), 'plot\Regression1\DistLoadingvar1PC1.png'))
-    loadings.append(os.path.join(os.path.dirname(__file__), 'plot\Regression\DistLoadingvar1PC2.png'))
-    loadings.append(os.path.join(os.path.dirname(__file__), 'plot\Regression\DistLoadingvar1PC3.png'))
+    pca = []
+    pcs = [[82.7, 13.4, 2.0],[98.9, 0.8, 0.1], [98.6, 0.9, 0.3]]
+    name = ['variable A', 'variable B', 'variable C']
+    print(len(pcs))
+    for i in range(nb):
+        #loadings.append(os.path.join(os.path.dirname(__file__), 'plot\Regression1\DistLoadingvar1PC' + str(i+1) +'.png'))
+        pca.append(os.path.join(os.path.dirname(__file__), 'plot\PCArepresentation\PCsVar' + str(i+1) +'.png'))
     
     # Basic document
     geometry_options = {
@@ -41,6 +74,6 @@ if __name__ == '__main__':
         "bottom": "0.6in"
     }
     doc = Document('report/basic', geometry_options=geometry_options)
-    fillPca(doc, loadings)
+    fillPca(doc, loadings, pcs, name)
 
     doc.generate_pdf(clean_tex=False)
